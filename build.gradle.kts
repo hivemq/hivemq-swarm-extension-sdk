51ca8582017fc14b528f7b6116f18c4e143f3fe0
Silvio Giebl <silvio.giebl@hivemq.com>
2021-08-16 14:43:48 +0200

    build > fix deprecation warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,8 +105,7 @@ tasks.javadoc {
 
     doLast {
         javaexec {
-            main = "-jar"
-            args("$projectDir/gradle/tools/javadoc-cleaner-1.0.jar")
+            classpath(projectDir.resolve("gradle/tools/javadoc-cleaner-1.0.jar"))
         }
     }
 
