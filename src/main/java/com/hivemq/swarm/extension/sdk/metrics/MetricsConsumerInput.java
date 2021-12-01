38912f5e9bc96d9be00158890f265ac22b26b268
Adrian Sterr <adriansterr@gmail.com>
2021-12-01 15:37:37 +0100

    should create simple pdf but doesn't (?)

--- src/main/java/com/hivemq/swarm/extension/sdk/metrics/MetricsConsumerInput.java
+++ src/main/java/com/hivemq/swarm/extension/sdk/metrics/MetricsConsumerInput.java
@@ -1,7 +1,10 @@
 package com.hivemq.swarm.extension.sdk.metrics;
 
 import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
 
 public interface MetricsConsumerInput {
-    void createMaps();
+    @NotNull HashMap<String, Long> countersMap();
+    @NotNull HashMap<String, HashMap<String, Long>> metersMap();
+    @NotNull HashMap<String, String> timersMap();
 }
