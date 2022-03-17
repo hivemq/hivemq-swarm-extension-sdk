f3d19d2f739478d2fda902bb3079b364fe23bf9e
Adrian Sterr <adriansterr@gmail.com>
2022-03-17 17:56:54 +0100

    Everything working more or less

--- src/main/java/com/hivemq/swarm/extension/sdk/metrics/MetricsConsumerInput.java
+++ src/main/java/com/hivemq/swarm/extension/sdk/metrics/MetricsConsumerInput.java
@@ -1,10 +1,13 @@
 package com.hivemq.swarm.extension.sdk.metrics;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface MetricsConsumerInput {
     @NotNull HashMap<String, Long> countersMap();
     @NotNull HashMap<String, HashMap<String, Long>> metersMap();
     @NotNull HashMap<String, String> timersMap();
+    @NotNull ArrayList<Long> timerValues();
 }
